be6abd784f4d4ce8bcace3639368029537636ce9
NathAmd <nathanaelamd@gmail.com>
2024-07-19 10:09:35 +0200

    Auto push

--- assets/script/module/async.js
+++ assets/script/module/async.js
@@ -2,7 +2,7 @@ import { loadPage } from './function.js'
 
 export async function fetchPage(page, query) {
     try {
-        const response = await fetch("/assets/page/" + page)
+        const response = await fetch("unityEngine-webgl-tuto/assets/page/" + page)
         if (response.ok) {
             print(response.text())
             // const html = await response.text()
