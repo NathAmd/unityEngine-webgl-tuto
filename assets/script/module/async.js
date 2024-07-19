dcebea940bc26665539c074cdefe54595c81426d
NathAmd <nathanaelamd@gmail.com>
2024-07-19 10:03:52 +0200

    Auto push

--- assets/script/module/async.js
+++ assets/script/module/async.js
@@ -4,8 +4,9 @@ export async function fetchPage(page, query) {
     try {
         const response = await fetch("/assets/page/" + page)
         if (response.ok) {
-            const html = await response.text()
-            loadPage(html, query)
+            print(response.text())
+            // const html = await response.text()
+            // loadPage(html, query)
         } else {
             throw new Error('Erreur lors de la récupération de la page')
         }
